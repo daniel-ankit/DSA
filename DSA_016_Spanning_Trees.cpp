b1f0407d314d6c267591651bc62b3fcc2c4c79c1
Ankit Kumar <ankit.kumar.061000@gmail.com>
2021-12-12 01:37:48 +0530

    Spanning Trees Update

--- DSA_016_Spanning_Trees.cpp
+++ DSA_016_Spanning_Trees.cpp
@@ -15,8 +15,55 @@ SPANNING TREE : A connected Subgraph S og graph G is said to be spanning tree if
     - Total Number of Spanning trees for a graph : n⁽ⁿ⁻²⁾.
 */
 
+class Graph
+{
+    public :
+    int N=0;
+    int **M;
+    bool *V;
+    Graph(int n)
+    {
+        this->N = n;
+        M = new int*[n];
+        for(int i=0; i<n; i++)
+        {
+            M[i] = new int[n];
+            for(int j=0; j<N; j++)
+                M[i][j] = -1;
+        }
+        V = new bool[n];
+        memset(V, 0, n);
+    }
+    void link(int a, int b, int cost)
+    {
+        M[a][b] = cost;
+        M[b][a] = cost;
+    }
+    void printmatrix()
+    {
+        for(int i=0; i<N; i++)
+        {
+            for(int j=0; j<N; j++)
+                cout << setw(2) << M[i][j] << " ";
+            cout << endl;
+        }
+    }
+    void minimumcost(int Starting_Node)
+    {
+        
+    }
+};
+
 int main()
 {
-    
+    Graph G(4);
+    G.link(0, 1, 2);
+    G.link(0, 2, 3);
+    G.link(0, 3, 6);
+    G.link(1, 2, 8);
+    G.link(1, 3, 1);
+    G.link(2, 3, 15);
+    G.printmatrix();
+
     return 69;
 }
---
+++ DSA_017_Sorting_Algorithms.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> &V)
+{
+    for(auto x : V)
+        cout << x << " ";
+    cout << endl;
+}
+
+void bubblesort(vector<int> &V)
+{
+    for(int i=0; i<V.size(); i++)
+    {
+        for(int j=i; j<V.size(); j++)
+            if(V[i]>V[j]) swap(V[i], V[j]);
+    }
+}
+
+void insertionsort(vector<int> &V)
+{
+
+}
+
+void quicksort(vector<int> &V)
+{
+
+}
+
+void mergesort(vector<int> &V)
+{
+
+}
+
+void countsort(vector<int> &V)
+{
+
+}
+
+int main()
+{
+    int Sample[5] = {15, 20, 10, 0, 5};
+    vector<int> V;
+    for(int i=0; i<5; i++)
+        V.push_back(Sample[i]);
+
+    bubblesort(V);
+    print(V);
+    return 69;
+}
