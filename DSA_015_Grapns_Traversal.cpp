7dd366bb80005368f477385c5bb20469ebf88973
Ankit Kumar <ankit.kumar.061000@gmail.com>
2021-12-12 00:09:38 +0530

    DFS Implementation

--- DSA_015_Grapns_Traversal.cpp
+++ DSA_015_Grapns_Traversal.cpp
@@ -15,10 +15,12 @@ GRAPH TRAVERSAL ALGORITHMS ARE:
 
 int N = 10;
 bool M[10][10] = {0};
+bool Visited[10] ={0};
 
 void link(int a, int b)
 {
     M[a][b]=1;
+    M[b][a]=1;
 }
 
 void printmatrix()
@@ -33,27 +35,20 @@ void printmatrix()
 
 void IntiateLinks()
 {
+    link(8, 9);
+    link(9, 7);
+    link(9, 0);
+    link(9, 4);
     link(7, 1);
     link(7, 2);
     link(7, 3);
-    link(1, 2);
-    link(1, 7);
-    link(2, 1);
-    link(2, 7);
-    link(2, 3);
     link(7, 6);
-    link(3, 2);
+    link(1, 2);
+    link(2, 3);
+    link(2, 4);
     link(3, 4);
-    link(4, 2);
-    link(4, 3);
     link(4, 5);
     link(4, 0);
-    link(5, 4);
-    link(0, 4);
-    link(9, 7);
-    link(9, 0);
-    link(4, 9);
-    link(8, 9);
 }
 
 set<int> BFS()
@@ -70,12 +65,12 @@ set<int> BFS()
     return V;
 }
 
-void BFSQ(int starting_Node)
+void BFS(int starting_Node)
 {
     queue <int> Q;
+    bool Visited[N] = {0};
     cout << starting_Node <<" ";
     Q.push(starting_Node);
-    bool Visited[N]={0}; // Initially all vertices are unvisited
     Visited[starting_Node] = true; // First given vertice is assumed to be present
     while(!Q.empty())
     {
@@ -93,19 +88,25 @@ void BFSQ(int starting_Node)
     }
 }
 
-void DFSS(int Starting_Node)
+void DFS(int Starting_Node)
 {
-
+    cout << Starting_Node << " ";
+    Visited[Starting_Node] = 1;
+    for(int j=0; j<N; j++)
+    {
+        if(M[Starting_Node][j]==1 && !Visited[j])
+            DFS(j);
+    }
 }
 
 int main()
 {
     IntiateLinks();
     printmatrix();
-    set <int> V = BFS();
-    set <int> :: iterator it;
-    for(it = V.begin(); it!=V.end(); it++)
-        cout << *it << " ";
-    BFSQ(2);
+    // set <int> V = BFS();
+    // for(auto x: V)
+    //     cout << x << " ";
+    BFS(2);
+    DFS(8);
     return 69;
 }
