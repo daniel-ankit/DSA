1a23684cfbb6382107b91c5944e1bf9ccfa04216
Ankit Kumar <ankit.kumar.061000@gmail.com>
2021-12-14 00:28:06 +0530

    Pointers Declaration and Uses

--- DSA_001_Pointers.cpp
+++ DSA_001_Pointers.cpp
@@ -2,6 +2,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*  THEORY
+
+Pointers store address of variables or a memory location.
+
+To access address of a variable to a pointer : add & (ampersand)
+To declare a pointer variable or to access vale stored in the address : add asterisk (*) before its name.
+
+*/
+
 int* create()
 {  
     int num;
--- DSA_017_Sorting_Algorithms.cpp
+++ DSA_017_Sorting_Algorithms.cpp
@@ -11,10 +11,32 @@ void print(vector<int> &V)
 
 void bubblesort(vector<int> &V)
 {
-    for(int i=0; i<V.size(); i++)
+
+    //   O(n²) Time Complexity
+    //   O(1) Auxiliary Space
+    //   O(n) Swaps
+
+    for(int i=0; i<V.size()-1; i++)
     {
-        for(int j=i; j<V.size(); j++)
-            if(V[i]>V[j]) swap(V[i], V[j]);
+        for(int j=0; j<V.size()-i-1; j++)
+            if(V[j+1] < V[j]) swap(V[j+1], V[j]);
+    }
+}
+
+void selectionsort(vector<int> &V)
+{
+
+    //   O(n²) Time Complexity
+    //   O(1) Auxiliary Space
+    //   O(n) Swaps
+
+    int index, i, j;
+    for(i=0; i<V.size()-1; i++)
+    {
+        index = i;
+        for(j=i+1; j<V.size(); j++)
+            if(V[j] < V[index]) index = j;
+        swap(V[i], V[index]);
     }
 }
 
@@ -40,12 +62,12 @@ void countsort(vector<int> &V)
 
 int main()
 {
-    int Sample[5] = {15, 20, 10, 0, 5};
+    int Sample[6] = {15, 20, 10, 0, 5, 0};
     vector<int> V;
-    for(int i=0; i<5; i++)
+    for(int i=0; i<6; i++)
         V.push_back(Sample[i]);
 
-    bubblesort(V);
+    selectionsort(V);
     print(V);
     return 69;
 }
