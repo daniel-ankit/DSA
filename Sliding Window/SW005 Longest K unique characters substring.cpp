290e666fcdc8fc46373ce1c8f7c8a939130bd573
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-03-05 23:42:19 +0530

    Variable Size Sliding Window

---
+++ Sliding Window/SW005 Longest K unique characters substring.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int longestKSubstr(string s, int k) {
+    map<char, int> M;
+        int j = 0, i = 0;
+        int ans = -1;
+        while(j<s.size())
+        {
+            M[s[j]]++;
+            if(M.size()==k)
+                ans = max(ans, j-i+1);
+            else if(M.size()>k)
+            {
+                while(M.size()>k)
+                {
+                    M[s[i]]--;
+                    if(M[s[i]]==0) M.erase(s[i]);
+                    i++;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution object;
+    string S = "aaaa";
+    int K = 2;
+    cout << object.longestKSubstr(S, K);
+    return 69;
+}
