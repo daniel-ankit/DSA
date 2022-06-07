59d46063870061d1e52a387f9d7ca1adbad9428c
Ankit Kumar <ankit.kumar.061000@gmail.com>
2022-06-07 18:15:30 +0530

    Initial Commit

---
+++ DSA_020_Graph.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    vector<Node*> adj;
+    Node(int num, vector<Node*> links)
+    {
+        
+    }
+};
+
+int main()
+{
+    return 0;
+}
--- Dynamic Programming/DP17 Longest Common Subsequence Bottom Up.cpp
+++ Dynamic Programming/DP17 Longest Common Subsequence Bottom Up.cpp
@@ -8,12 +8,9 @@ int LongestCommonSubsequence(string A, int a, string B, int b)
     if(a==0 || b==0) return 0;
     if(DP[a][b]!=-1) return DP[a][b];
     if(A[a-1] == B[b-1])
-    {
-        DP[a][b] = 1+LongestCommonSubsequence(A, a-1, B, b-1);
-        return 1+LongestCommonSubsequence(A, a-1, B, b-1);
-    }
-    DP[a][b] = max(LongestCommonSubsequence(A, a-1, B, b), LongestCommonSubsequence(A, a, B, b-1));
-    return max(LongestCommonSubsequence(A, a-1, B, b), LongestCommonSubsequence(A, a, B, b-1));
+        DP[a][b] = 1 + LongestCommonSubsequence(A, a-1, B, b-1);
+    else DP[a][b] = max(LongestCommonSubsequence(A, a-1, B, b), LongestCommonSubsequence(A, a, B, b-1));
+    return DP[a][b];
 }
 
 int main()
